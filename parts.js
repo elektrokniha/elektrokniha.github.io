3b7fad9ad51ef4c0322849226dd827978da8d186
Martin Maly <maly@maly.cz>
2018-01-28 15:37:45 +0100

    Shops by JS

---
+++ parts.js
@@ -0,0 +1,172 @@
+var parts = {
+
+
+	hwk: {
+		prefix: 'https://www.hwkitchen.cz/',
+		name: '<span class="fa fa-thumbs-up"></span> HW Kitchen',
+		items: {
+			uno: 'arduino-uno-rev3/',	
+			nkp: 'kontaktni-pole/',
+			dupont: 'propojovaci-vodice/',
+			pwusb: 'univerzalni-zdroj-nabijecka-2x-usb-5v-3-1a-s-kabely/',
+			seg7: '7-segmentovy-led-displej-zeleny/',
+			hcsr04: '',
+			moist: '',
+			pir: 'BEmEaqn',
+			max7219: '',
+			lm75a: '',
+			dht22: 'snimac-teploty-a-vlhkosti-dht22/',
+	
+		}
+	},
+
+	bastlime: {
+		prefix: 'https://www.bastlime.eu/',
+		name: '<span class="fa fa-credit-card"></span> Bastlíme.eu',
+		items: {
+			uno: '#CZ7006685783',	
+			nkp: '#CZ7006686556',
+			dupont: '#CZ7006686576',
+			led: '#CZ7006686548',
+			res: '#CZ7006686594',
+			cap: '#CZ7006886472',
+			sn7404: '#CZ7006886466',
+			hcsr04: '#CZ7006686584',
+			lm75a: '#CZ7006886467',
+
+		}
+	},
+
+
+	ali: {
+		prefix: 'http://s.click.aliexpress.com/e/',
+		name: '<span class="fa fa-shopping-bag"></span> AliExpress',
+		items: {
+			nkp: 'ieau3nY',
+			dupont: 'Q3na2JU',
+			nkppwr: 'qz7YJyf',
+			pwusb: 'qzfyjAi',
+			pw7: '7YNzrja',
+			metr: 'E276mmu',
+			led: 'UfqfuZj',
+			seg7: 'bQz7UzB',
+			res: 'yVjqVNz',
+			cap: 'RneYfQf',
+			diode: 'EAEQbQF',
+			bc337: '',
+			bc557: '',
+			n3904: '',
+			n3906: '',
+			tip120: '',
+			sn7400: '',	
+			sn7402: '',	
+			sn7486: '',	
+			sn7404: '',	
+			sn7414: '',	
+			sn7405: '',	
+			sn7408: '',	
+			a2401: 'yzZbUzV',
+			uno: 'naeAMRZ',	
+			node: '7yRzb6y',
+			hcsr04: 'ufqv7yf',
+			moist: 'ZZf6QFy',
+			pir: 'BEmEaqn',
+			max7219: 'urniAII',
+			lm75a: 'AQz7UJm',
+			dht22: 'QZrvJmm',
+			sht30: 'Bi2bYje',
+			bmp180: 'BM72fA6',
+			rf433: 'rBEiaiA',
+			nrf24: 'v3Nfqji',
+			nrf24k: 'QzBam2f',
+			lcd1602: 'BEyJauB',
+			i2c1602: 'BEyJauB',
+			oled12864: 'E6mUFMj',
+			joystick: 'vRzfme2',
+			rotary: 'EUvvZRR',
+		}
+	},
+
+
+	gme: {
+		prefix: 'https://www.gme.cz/',
+		name: '<span class="fa fa-shopping-cart"></span> GME</a>',
+		items: {
+			nkp: 'nepajiva-kontaktni-pole',
+			dupont: 'prislusenstvi-pro-nepajiva-kontaktni-pole',
+			nkppwr: 'napajeci-modul-kontaktniho-pole',
+			pwusb: '',
+			metr: 'digitalni-multimetry-category-2209',
+			led: 'led-kulate-pouzdro?price=1;58&tech_par[542][]=10293&showFilter=542&page=1#products',
+			seg7: 'led-displeje',
+			res: 'sady-rezistoru',
+			cap: 'kondenzatory',
+			diode: 'univerzalni-a-rychle-diody',
+			bc337: 'bipolarni-tranzistor-bc337-25-to92',
+			bc557: 'bipolarni-tranzistor-bc557b-to92',
+			n3904: 'bipolarni-tranzistor-2n3904-to92-ammo',
+			n3906: 'bipolarni-tranzistor-2n3906-ark-ammo',
+			tip120: 'darlingtonuv-tranzistor-tip120-to220',
+			sn7400: '74hct00-dip14-texas-instruments',	
+			sn7402: '74hct02-dip14-texas-instruments',	
+			sn7486: '74hct86-dip14-texas-instruments',	
+			sn7404: '74hct04-dip14-texas-instruments',	
+			sn7414: '74hct14-dip14-texas-instruments',	
+			sn7405: '',	
+			sn7408: '74hct08-dip14-texas-instruments',	
+			sn7474: '74hct74-dip14-texas-instruments',	
+			sn7475: '74hc75-dip16',	
+			sn7490: '',	
+			sn7493: '74hct93-dip14-texas-instruments',	
+			sn74138: '74hct138-dip16-texas-instruments',	
+			sn74595: '74hc595-dip16-texas-instruments',	
+			sn74162: '74hct162-dip16-philips',	
+			a2401: '24c01',
+			node: 'nodemcu-lua-esp8266-ch340-wi-fi',
+			hcsr04: 'modul-ultrazvukoveho-merice-vzdalenosti',
+			moist: '',
+			pir: 'pir-modul-sb00612a',
+			max7219: 'maticovy-led-displej-8x8-s-max7219',
+			lm75a: '',
+			dht22: 'digitalni-teplomer-a-vlhkomer-dht22',
+			sht30: '',
+			bmp180: 'modul-meteocidlo-bmp180',
+			rf433: 'modul-vysilac-prijimac-433mhz',
+			nrf24: 'modul-wifi-s-nrf24l01',
+			nrf24k: 'shield-pro-nrf24l01-s-atmega48',
+			lcd1602: 'lcd-alfanumericky-displej-winstar-wh1602a-nyg-et',
+			i2c1602: 'prevodnik-i2c-display',
+			oled12864: 'oled-display-0-96-128x64',
+			joystick: 'modul-s-jednim-analogovym-joystickem',
+			rotary: 'modul-s-rotacnim-enkoderem',
+		}
+	},
+
+	tme: {
+		prefix: 'https://www.tme.eu/cz/details/',
+		name: '<span class="fa fa-truck"></span> TME</a>',
+		items: {
+			sn7490: 'sn74ls90n/citacedelice/texas-instruments/',	
+		}
+	},
+
+
+};
+
+var getbuy = function(id) {
+	var out = [];
+	for(var shop in parts) {
+		if (parts[shop].items[id]) {
+			out.push('<a href="'+parts[shop].prefix+parts[shop].items[id]+'">'+parts[shop].name+'</a>');
+		}
+	}
+	return out.join(' | ');
+}
+
+$("#content ul li span").each( function() {
+		var id = $(this).attr("id");
+		var links = getbuy(id);
+		$("#"+id).html(links)
+	}
+)
+
